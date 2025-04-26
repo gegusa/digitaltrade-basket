86b2e50cb93ecd37a93b4aa58f84031cd5e47586
gegusa <gegusaa@yandex.ru>
2025-04-26 17:34:40 +0300

    добавил зависимости

---
+++ DigitalTrade.Basket.Migrations/Scripts/Initial/20250426172527_create_table_shopping_cart.sql
@@ -0,0 +1,15 @@
+create schema basket
+
+    CREATE TABLE if not exists basket.shopping_cart_items
+    (
+        id              BIGSERIAL         PRIMARY KEY,
+        user_id         BIGSERIAL         NOT NULL,                      -- Идентификатор пользователя
+        product_id      BIGSERIAL         NOT NULL,                      -- Идентификатор товара
+        quantity        INT               NOT NULL CHECK (quantity > 0), -- Количество товара
+        price_at_adding NUMERIC(10, 2)    NOT NULL,                      -- Цена товара на момент добавления
+        added_at        TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),    -- Когда добавили в корзину
+        updated_at      TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),    -- Когда обновили (например, изменили количество)
+
+        -- Ограничение: у одного пользователя не может быть несколько одинаковых товаров в корзине
+        CONSTRAINT unique_user_product UNIQUE (user_id, product_id)
+    );
